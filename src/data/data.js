19904eb9d8fa751926c27a30bbb27ecce0b5519c
oliceglad <vycheslavofficial@gmail.com>
2024-04-13 16:58:35 +0400

    changed data lang

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,17 @@
 export const dataNames = {
-    "sculpture": "скульптура"
+    "sculpture": "Скульптура",
+    "books": "Книга",
+    "archaeology": "Археология",
+    "decorative-and-applied-arts": "ДПИ",
+    "documents": "Документы",
+    "graphics": "Графика",
+    "mineralogy": "Минералогия",
+    "natural-science-collection": "Естественно-научная коллекция",
+    "numismatics": "Нумизматика",
+    "other": "Прочее",
+    "painting": "Живопись", 
+    "photographic-negatives": "Фото, негатив",
+    "printed-matter": "Печатная продукция",
+    "technology": "Техника", 
+    "weapons": "Оружие"
 }
